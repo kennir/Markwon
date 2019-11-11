4612b0eadfeead53f0835e184a190f0b11eff93d
靖宇 张 <kennir@icloud.com>
2019-11-11 17:34:41 +0800

    android x

--- sample/src/main/java/ru/noties/markwon/sample/recycler/RecyclerActivity.java
+++ sample/src/main/java/ru/noties/markwon/sample/recycler/RecyclerActivity.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.LinearLayoutManager;
+import androidx.appcompat.widget.RecyclerView;
 import android.text.TextUtils;
 
 import org.commonmark.ext.gfm.tables.TableBlock;
